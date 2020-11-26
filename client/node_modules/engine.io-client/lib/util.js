ca85729aabc76f0c86dc077e1b3623a604c7c9c6
Benoit Paul <benoitpaul@Benoits-MacBook-Pro.local>
2020-11-26 12:56:42 -0500

    removed client node_modules

--- client/node_modules/engine.io-client/lib/util.js
+++
@@ -1,6 +0,0 @@
-module.exports.pick = (obj, ...attr) => {
-  return attr.reduce((acc, k) => {
-    acc[k] = obj[k];
-    return acc;
-  }, {});
-};
--- client/node_modules/socket.io-client/build/url.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * URL parser.
- *
- * @param {String} uri - url
- * @param {Object} loc - An object meant to mimic window.location.
- *                 Defaults to window.location.
- * @public
- */
-export declare function url(uri: any, loc?: Location): any;
